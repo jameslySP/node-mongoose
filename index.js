dc6b1ffd45b52302bce80a712ac3df024ce9a6dd
jamesquietly <jamesquietly@gmail.com>
2018-11-25 18:43:53 -0800

    added comments to dishes

--- index.js
+++ index.js
@@ -16,11 +16,25 @@ connect.then((db) => {
     .then((dish) => {
         console.log(dish);
 
-        return Dishes.find({}).exec();
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test' }
+        }, { 
+            new:true
+        }).exec();
     })
-    .then((dishes) => {
-        console.log(dishes);
+    .then((dish) => {
+        console.log(dish);
 
+        dish.comments.push({
+            rating: 5,
+            comment: 'This dish is good',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
         return Dishes.deleteOne({});
     })
     .then(() => {
